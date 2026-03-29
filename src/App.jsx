0ad30ce20c450c973e121afc0c59ced0be2d4aa5
sadik111222 <md8413226@gmail.com>
2026-03-29 21:02:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import NavBar from './NavBar/NavBar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <NavBar></NavBar>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='flex justify-between items-center px-5 lg:px-10 py-7'>
+            <div className='text-5xl font-bold '>
+                <h1>DigiTools</h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default NavBar;
